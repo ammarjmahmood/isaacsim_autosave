9d88e845f8c1924ce5b5e4a59401441bb34b783e
Ammar Jaleel Mahmood <amarsbar@gmail.com>
2026-04-14 21:56:16 -0400

    Add simulation check: skip saving while playing to avoid persisting mid-motion poses

--- exts/omni.autosave/omni/autosave/extension.py
+++ exts/omni.autosave/omni/autosave/extension.py
@@ -3,6 +3,7 @@ import carb
 import omni.ext
 import omni.usd
 import omni.kit.app
+import omni.timeline
 
 
 COOLDOWN = 5.0
@@ -11,6 +12,7 @@ COOLDOWN = 5.0
 class AutoSaveExtension(omni.ext.IExt):
     def on_startup(self, ext_id):
         self._last_save = time.time()
+        self._timeline = omni.timeline.get_timeline_interface()
 
         self._update_sub = (
             omni.kit.app.get_app()
@@ -22,9 +24,14 @@ class AutoSaveExtension(omni.ext.IExt):
 
     def on_shutdown(self):
         self._update_sub = None
+        self._timeline = None
 
     def _on_update(self, event):
         if time.time() - self._last_save < COOLDOWN:
+            return
+
+        # Skip saving if simulation is currently playing to avoid persisting mid-motion poses
+        if self._timeline.is_playing():
             return
 
         ctx = omni.usd.get_context()
